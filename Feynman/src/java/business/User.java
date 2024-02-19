2825a6ecc1b5d810cf91d367e8a72d23491f9192
jw536419 <jw536419@LR002D02.southeast.edu>
2024-02-19 13:57:42 -0600

    Basic User class skeleton.

--- Feynman/src/java/business/User.java
+++ Feynman/src/java/business/User.java
@@ -1,14 +1,63 @@
 package business;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.io.Serializable;
 
-/**
- *
- * @author jw536419
- */
-public class User {
-    
+public class User implements Serializable {
+    String username;
+    String password;
+    Integer roleID;
+    String firstName;
+    String lastName; 
+
+    public User(String username, String password, Integer roleID, String firstName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.roleID = roleID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getRoleID() {
+        return roleID;
+    }
+
+    public void setRoleID(Integer roleID) {
+        this.roleID = roleID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+   
+    public String getFullName() {
+        return String.format("%s %s", this.firstName, this.lastName);
+    }    
 }
