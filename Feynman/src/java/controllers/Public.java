8f49401f8d5efa128c7db056de659ed5f087d3ef
im757299 <im757299@LR002D03.southeast.edu>
2024-02-21 13:23:22 -0600

    updated public controller to redirect to a jsp

--- Feynman/src/java/controllers/Public.java
+++ Feynman/src/java/controllers/Public.java
@@ -28,22 +28,8 @@ public class Public extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Public</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Public at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -69,7 +55,9 @@ public class Public extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String url = "/index.jsp";
+        
+        getServletContext().getRequestDispatcher(url).forward(request, response);
     }
 
     /**
@@ -80,6 +68,5 @@ public class Public extends HttpServlet {
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
-
+    }
 }
--- Feynman/web/index.jsp
+++ Feynman/web/index.jsp
@@ -1,8 +1,11 @@
+<%-- 
+    Document   : index
+    Created on : Feb 21, 2024, 1:19:40 PM
+    Author     : im757299
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Html.html to edit this template
--->
 <html>
     <head>
         <title>TODO supply a title</title>
