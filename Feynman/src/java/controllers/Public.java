4a1f1eb26b2f1b563efe0ab2eafe007be8921422
jw536419 <jw536419@LR002D02.southeast.edu>
2024-02-26 13:47:40 -0600

    Logging in checkpoint 2

--- Feynman/src/java/controllers/Public.java
+++ Feynman/src/java/controllers/Public.java
@@ -42,7 +42,29 @@ public class Public extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+String url = "/index.jsp";
+        String action = request.getParameter("action");
+        ArrayList<String> errors = new ArrayList();
+        String message = "";
+        
+        if (action == null) {
+            action = "default";
+        }
+        
+        switch (action) {
+            case "default":
+                break;
+            case "login":
+                url="/login.jsp";
+                break;
+            case "register":
+                url = "/registration.jsp";
+                break;
+        }
+        
+        request.setAttribute("errors", errors);
+      
+        getServletContext().getRequestDispatcher(url).forward(request, response);
     }
 
     /**
@@ -83,11 +105,11 @@ public class Public extends HttpServlet {
                 if (user != null) {
                     HttpSession session = request.getSession();
                     session.setAttribute("user", user);
-                    url="index.jsp";
+                    url="/index.jsp";
                 } else {
                     message = "Login Unsuccessful";
                     request.setAttribute("message", message);
-                    url="login.jsp";
+                    url="/login.jsp";
                 }
                 break;
             case "register":
